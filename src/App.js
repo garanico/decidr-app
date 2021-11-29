9e5c817d0528cfe47bb1cd1c7c5294bf831d2072
Grace Aranico <grace.aranico@gmail.com>
2021-11-29 14:45:07 -0800

    feat(input form): Create input form #6

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import { useState } from 'react';
+
+//components
 import Header from './components/Header';
+import InputForm from './components/InputForm';
 
 function App() {
   const [listContainer, setListContainer] = useState(["item 1", "item 2", "item 3"]);
@@ -7,6 +10,7 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <InputForm />
     </div>
   );
 }
---
+++ src/components/InputForm.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function InputForm() {
+    return (
+        <>
+        <form>
+            <input type="text" placeholder="Enter your choices..." />
+            <button>Add Choice</button>   
+        </form>
+        </>
+    )
+}
+
+export default InputForm
