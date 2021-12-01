12ec624900db2b3b8c1a8ad6bfcda01eae520890
Grace Aranico <grace.aranico@gmail.com>
2021-11-30 17:28:56 -0800

    feat(header): create header #12

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ function Header() {
     return (
         <>
         <header className="App-header">
-        <h1>Decidr</h1>
+        <h1>decidr.</h1>
         <p>An app for indecisive people.</p>
       </header>
       </>
