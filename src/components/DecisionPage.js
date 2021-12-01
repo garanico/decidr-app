ab64231ef9c6f4e2a4d6bcbd57aae250361bfe73
Grace Aranico <grace.aranico@gmail.com>
2021-11-30 22:04:36 -0800

    feat(decision page): create decision page #10

---
+++ src/components/DecisionPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from './Header';
+
+function DecisionPage() {
+    return (
+        <div>
+           <Header /> 
+        </div>
+    )
+}
+
+export default DecisionPage
